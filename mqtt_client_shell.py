e95e0152a6183de63977911cd641fb47a968484d
Brad Powell <bapowell@sbcglobal.net>
2016-06-12 18:55:05 -0500

    Added more robust subscription handling

--- mqtt_client_shell.py
+++ mqtt_client_shell.py
@@ -385,8 +385,7 @@ class ConnectionConsole(RootConsole):
 
     def do_connect(self, arg):
         """Connect to the MQTT server, using the current connection parameters.
-        If connection successful, then go to the "Messaging" console.
-        """
+        If connection successful, then go to the "Messaging" console."""
         connected = None
         try:
             if self.context.connection_args.username:
@@ -466,7 +465,7 @@ class MessagePublisher(object):
 
     def __init__(self, mqttclient):
         """Initialize, with an MQTT client instance.
-        A message sequence counter is also reset (incremented each time a message is published)."""
+        A message sequence counter is also reset (incremented each time a message is successfully published)."""
         self._mqttclient = mqttclient
         self._msg_seq = 1
 
@@ -482,11 +481,12 @@ class MessagePublisher(object):
                 payload = payload.format(seq=self._msg_seq)
             (result, msg_id) = self._mqttclient.publish(topic=topic, payload=payload, qos=qos, retain=retain)
             print("...msg_id={!r}, result={} ({})".format(msg_id, result, mqtt.error_string(result)))
-            self._msg_seq += 1
+            if result == mqtt.MQTT_ERR_SUCCESS:
+                self._msg_seq += 1
 
     def parse_publish(self, line):
-        """Publish a message, after parsing the parameters from the given string,
-        which should be formatted as follows:
+        """Publish a message, after parsing the parameters from the given string, which
+        should be formatted as follows:
             topic  payload  [qos  [retain]]
         topic and payload can be quoted (e.g. contain spaces)
         qos (0, 1, or 2) is optional; defaults to 0
@@ -494,6 +494,70 @@ class MessagePublisher(object):
         (topic, payload, qos, retain) = self.parse_pub_msg_input(line)
         self.publish(topic=topic, payload=payload, qos=qos, retain=retain)
         
+
+class SubscriptionHandler(object):
+    """Assist with MQTT subscriptions.
+    """
+    
+    @staticmethod
+    def parse_topic_sub_input(line):
+        """Parse a space-delimited line of text designating the parameters for a topic subscription: topic qos
+        topic (string) - may be quoted, e.g. if contains whitespace
+        qos (integer, optional: defaults to 0)
+        """
+        (topic, qos_str) = (shlex.split(line) + [None]*2)[:2]
+        qos = 0
+        if qos_str:
+            if qos_str.isdigit() and (0 <= int(qos_str) <= 2):
+                qos = int(qos_str)
+            else:
+                print("Invalid QoS value; should be 0, 1, or 2. Defaulting to 0.")
+        return (topic, qos)    
+
+    def __init__(self, mqttclient):
+        """Initialize, with an MQTT client instance.
+        A set, to contain active subscription topics, is also created."""
+        self._mqttclient = mqttclient
+        self._subscription_topics = set()
+
+    def subscribe(self, topic, qos=0):
+        """Subscribe to a topic."""
+        if not topic:
+            print("Topic must be specified")
+        else:
+            (result, msg_id) = self._mqttclient.subscribe(topic=topic, qos=qos)
+            print("...msg_id={!r}, result={} ({})".format(msg_id, result, mqtt.error_string(result)))
+            if result == mqtt.MQTT_ERR_SUCCESS:
+                self._subscription_topics.add(topic)
+
+    def parse_subscribe(self, line):
+        """Subscribe to a topic, after parsing the parameters from the given string, which
+        should be formatted as follows:
+            topic [qos]
+        topic can be quoted (e.g. contain spaces)
+        qos (0, 1, or 2) is optional; defaults to 0"""
+        (topic, qos) = self.parse_topic_sub_input(line)
+        self.subscribe(topic=topic, qos=qos)
+        
+    def unsubscribe(self, topic):
+        """Unsubscribe from a topic."""
+        if not topic:
+            print("Topic must be specified")
+        else:
+            (result, msg_id) = self._mqttclient.unsubscribe(topic)
+            print("...msg_id={!r}, result={} ({})".format(msg_id, result, mqtt.error_string(result)))
+            if result == mqtt.MQTT_ERR_SUCCESS:
+                self._subscription_topics.discard(topic)
+
+    def unsubscribe_all(self):
+        """Unsubscribe from all active subscriptions."""
+        for t in self._subscription_topics.copy():
+            self.unsubscribe(t)
+
+    def subscription_topics_str(self):
+        """Return a comma-separated list of the currently active subscriptions."""
+        return ', '.join([str(t) for t in self._subscription_topics])
+
 
 class MessagingConsole(RootConsole):
     """Console for performing pub/sub messaging, via an active MQTT connection.
@@ -503,6 +567,7 @@ class MessagingConsole(RootConsole):
         """Initialize, with a context."""
         RootConsole.__init__(self, context)
         self._msg_publisher = MessagePublisher(self.context.mqttclient)
+        self._sub_handler = SubscriptionHandler(self.context.mqttclient)
         self.update_prompt()
 
     def build_prompt(self):
@@ -510,6 +575,7 @@ class MessagingConsole(RootConsole):
         p = ""
         if self.context.prompt_verbosity == "H":
             p = ("\n***CONNECTED***" + 
+                 "\nSubscriptions: {}".format(self._sub_handler.subscription_topics_str()) + 
                  "\n{}".format(self.context.connection_args) + 
                  "\n{}".format(self.context.client_args))
         elif self.context.prompt_verbosity in ("M", "L"):
@@ -525,31 +591,37 @@ class MessagingConsole(RootConsole):
 
     def do_disconnect(self, arg):
         """Disconnect from the MQTT server.
-        Note that exiting this console session, via exit, quit, etc., will also disconnect. 
-        """
+        Note that exiting this console session, via exit, quit, etc., will also disconnect."""
         return True
 
     def do_publish(self, arg):
-        """Publish a message, e.g. publish  topic  payload  [qos  [retain]]
+        """Publish a message, e.g. publish topic payload [qos [retain]]
         topic can be quoted (e.g. contains spaces)
         payload can be quoted (e.g. contains spaces);
             if contains "{seq}", then published message sequence# will be substituted;
-            if starts with "from-url:", then contents of the specified resource will be used, e.g. from-url:file:///tmp/test.txt
+            if starts with "from-url:", then contents of the specified resource will be used, e.g. from-url:file:///home/fred/test.txt
         qos (0, 1, or 2) is optional; defaults to 0
         retain (true/false or yes/no) is optional; defaults to false"""
         self._msg_publisher.parse_publish(arg)
 
     def do_subscribe(self, arg):
-        """Subscribe to a topic"""
-        topic = arg
-        (result, msg_id) = self.context.mqttclient.subscribe(topic=topic, qos=0)
-        print("...msg_id={!r}, result={} ({})".format(msg_id, result, mqtt.error_string(result)))
+        """Subscribe to a topic, e.g. subscribe topic [qos]
+        topic can be quoted (e.g. contains spaces)
+        qos (0, 1, or 2) is optional; defaults to 0"""
+        self._sub_handler.parse_subscribe(arg)
 
     def do_unsubscribe(self, arg):
-        """Unsubscribe from a topic"""
-        topic = arg
-        (result, msg_id) = self.context.mqttclient.unsubscribe(topic)
-        print("...msg_id={!r}, result={} ({})".format(msg_id, result, mqtt.error_string(result)))
+        """Unsubscribe from a topic, e.g. unsubscribe topic
+        topic can be quoted (e.g. contains spaces)"""
+        self._sub_handler.unsubscribe(arg)
+
+    def do_unsubscribe_all(self, arg):
+        """Unsubscribe from all active topic subscriptions, e.g. unsubscribe_all"""
+        self._sub_handler.unsubscribe_all()
+
+    def do_list_subscriptions(self, arg):
+        """List the currently active topic subscriptions"""
+        print("Active topic subscriptions: {}".format(self._sub_handler.subscription_topics_str()))
 
 
 if __name__ == '__main__':
